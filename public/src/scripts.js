35e8422874b8bc6ddc136a493ceb3890d957a841
ejarzo <ejarz25@gmail.com>
2015-10-26 15:52:51 +0100

    improved scrolling

--- public/src/scripts.js
+++ public/src/scripts.js
@@ -7,9 +7,16 @@ $(document).ready(function(){
     });
 
     $(document).scroll(function() {
-        var o = ($(document).scrollTop() - 90)/ 200;
+        var scroll = $(document).scrollTop();
+        var o = (scroll - 90)/ 200;
         var bso = (o - 1) / 10
+        console.log("scroll:"+scroll);
+        var so = 1 - scroll / 200;
+        console.log("so:"+so);
+
         $("header").css("background","rgba(85, 85, 85, "+ o +")");
+        $("#splash-text").css("opacity",+ so);
+
 //        $("header").css("background","rgba(187, 196, 205, "+ o +")");
 //        $("header").css("box-shadow","0 0px 20px rgba(80,80,80,"+bso+")");
 
